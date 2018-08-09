0eed0b6f04c834d0377868cb32c76620a9502398
Gutierrez PS <gutierrez@live.jp>
2018-08-09 02:40:29 -0300

    Now updating regfile outputs on CLK rising edge

--- dpt_regfile.vhd
+++ dpt_regfile.vhd
@@ -5,35 +5,39 @@ use IEEE.NUMERIC_STD.all;
 
 entity dpt_regfile is
 port(
-	CLK, WE3		: in  STD_LOGIC;
-	A1, A2, A3	: in  STD_LOGIC_VECTOR(4 downto 0);
-	WD3			: in  STD_LOGIC_VECTOR(31 downto 0);
-	RD1, RD2		: out STD_LOGIC_VECTOR(31 downto 0)
+    CLK, WE3    : in  STD_LOGIC;
+    A1, A2, A3  : in  STD_LOGIC_VECTOR(4 downto 0);
+    WD3         : in  STD_LOGIC_VECTOR(31 downto 0);
+    RD1, RD2    : out STD_LOGIC_VECTOR(31 downto 0)
 );
 end dpt_regfile;
 
 architecture behave of dpt_regfile is
-	type ramtype is array (31 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
-	signal registers: ramtype;
+    type ramtype is array (31 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
+    signal registers: ramtype;
+    signal D1, D2: STD_LOGIC_VECTOR(31 downto 0);
 
 begin
-	process( CLK ) begin
-		if rising_edge(CLK) and WE3 = '1' then
-            registers(to_integer(unsigned(A3))) <= WD3;
+    process( CLK ) begin
+        if (rising_edge(CLK)) then
+            if (A1 = 0) then
+                D1 <= X"00000000";
+            else
+                D1 <= registers(to_integer(unsigned(A1)));
+            end if;
+
+            if (A2 = 0) then
+                D2 <= X"00000000";
+            else
+                D2 <= registers(to_integer(unsigned(A2)));
+            end if;
+
+            if (WE3 = '1') then
+                registers(to_integer(unsigned(A3))) <= WD3;
+            end if;
         end if;
     end process;
     
-    process( A1, A2, registers ) begin
-        if( A1 = 0 ) then
-            RD1 <= X"00000000";
-        else
-            RD1 <= registers(to_integer(unsigned(A1)));
-        end if;
-
-        if( A2 = 0 ) then
-            RD2 <= X"00000000";
-        else
-            RD2 <= registers(to_integer(unsigned(A2)));
-        end if;
-	end process;
+    RD1 <= D1;
+    RD2 <= D2;
 end;
