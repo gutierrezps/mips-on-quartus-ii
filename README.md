79c112f45e85c69c19f8eefe6fd7358c20ef7724
Gutierrez PS <gutierrez@live.jp>
2018-08-26 22:54:18 -0300

    Added testbench instructions, including mux2 tb file

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The multicycle microarchitecture is based on chapter 7 of Harris' *Digital Desig
 All software below can be downloaded for free at https://fpgasoftware.intel.com/13.0sp1/?edition=web
 
 * Altera Quartus II 13.0sp1 Web Edition
-* ModelSim
+* ModelSim (bundled with Quartus II 13.0)
 * Cyclone III device support
 
 ## Instructions
@@ -16,8 +16,14 @@ All software below can be downloaded for free at https://fpgasoftware.intel.com/
 3. Choose a top-level entity (optional): on Quartus II, Project Navigator, Files tab,
     right-click on the desired file and select "Set as Top-Level Entity"
 
+## Simulation/Testbench
+Since ModelSim doesn't create project files with relative paths, you will need to create a project
+and include the files manually. See Kashani-Akhavan's tutorial, sections 4 and 5.
+
 ## References
 * Harris, D., & Harris, S. (2010). *Digital Design and Computer Architecture*. Morgan Kaufmann.
+* Kashani-Akhavan, Sahand. *VHDL Testbench Tutorial*. https://moodle.epfl.ch/pluginfile.php/1833321/mod_resource/content/1/vhdl_testbench_tutorial.pdf
+* Nandland. *VHDL Tutorials and Examples*. https://www.nandland.com/vhdl/tutorials/index.html
 * Patterson, D. A., & Hennessy, J. L. (2008). *Computer Organization and Design:
     The Hardware/Software Interface*, 4th edition. Morgan Kaufmann.
 * Terasic DE0 Board - http://www.terasic.com.tw/cgi-bin/page/archive.pl?Language=English&CategoryNo=56&No=364
---
+++ sim/mux2_width_tb.vhd
@@ -0,0 +1,60 @@
+-- Multiplexer 2 to 1 (generic width) testbench
+--
+-- Author: Gutierrez PS / https://github.com/gutierrezps/mips-on-quartus-ii
+--
+-- Based on: VHDL Testbench Tutorial, by Sahand Kashani-Akhavan
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mux2_width_tb is
+end;
+
+architecture bench of mux2_width_tb is
+    constant c_TIME_DELTA : time := 100 ns;
+    constant g_WIDTH: integer := 32;
+
+    signal i_data0: std_logic_vector(g_WIDTH-1 downto 0);
+    signal i_data1: std_logic_vector(g_WIDTH-1 downto 0);
+    signal i_sel: std_logic;
+    signal o_data: std_logic_vector(g_WIDTH-1 downto 0) ;
+
+begin -- architecture bench
+
+    dut: entity work.mux2_width
+    port map (
+        i_data0 => i_data0,
+        i_data1 => i_data1,
+        i_sel   => i_sel,
+        o_data  => o_data
+    );
+
+    stimulus: process
+        procedure check_mux(
+            constant data0  : in std_logic_vector(g_WIDTH-1 downto 0);
+            constant data1  : in std_logic_vector(g_WIDTH-1 downto 0);
+            constant sel    : in std_logic;
+            constant data   : in std_logic_vector(g_WIDTH-1 downto 0))
+        is
+        begin
+            i_data0 <= data0;
+            i_data1 <= data1;
+            i_sel   <= sel;
+            wait for c_TIME_DELTA;
+
+            -- Check output against expected result.
+            assert o_data = data
+            report "mismatch" severity error;
+        end procedure check_mux;
+    
+    begin
+        check_mux(X"FFFFFFFF", X"00000000", '0', X"FFFFFFFF");
+        check_mux(X"FFFFFFFF", X"00000000", '1', X"00000000");
+        check_mux(X"00000000", X"FFFFFFFF", '0', X"00000000");
+        check_mux(X"00000000", X"FFFFFFFF", '1', X"FFFFFFFF");
+        wait;
+    end process;
+
+end architecture bench;
