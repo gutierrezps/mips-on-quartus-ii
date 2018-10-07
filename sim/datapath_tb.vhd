8cc7e1b3e8f5c6a782bd212597a80e369d27bf30
Gutierrez PS <gutierrez@live.jp>
2018-10-07 13:35:46 -0300

    Datapath tb: added procedure for states 0 and 1, added addi test instr

--- sim/datapath_tb.vhd
+++ sim/datapath_tb.vhd
@@ -64,7 +64,44 @@ begin
         );
 
     stimulus: process
-    begin
+
+        procedure states0and1(
+            constant instr  : in std_logic_vector(31 downto 0);     -- instruction
+            constant pc     : in std_logic_vector(31 downto 0))
+        is
+        begin
+            -- S0: fetch instruction
+            i_iOrD      <= '0';     -- set memAddr output to PC reg value
+            wait for c_PROP_DELAY;
+            assert o_memAddr = pc report "PC value mismatch" severity error;
+
+            i_irWrite   <= '1';     -- write to instr reg
+            i_aluSrcA   <= '0';     -- PC
+            i_aluSrcB   <= "01";    -- 4
+            i_aluControl <= "010";  -- add
+            i_pcSrc     <= "00";    -- aluOut
+            i_pcWrite   <= '1';     -- set new PC value (PC + 4)
+            i_readData  <= instr;   -- instruction "read" from mem
+
+            wait for c_CLOCK_PERIOD - c_PROP_DELAY;
+
+            i_irWrite   <= '0';     -- disable write signals
+            i_pcWrite   <= '0';
+
+            -- S1: decode instruction
+            i_aluSrcA   <= '0';     -- calculate branch address (PC + SignImmSl2)
+            i_aluSrcB   <= "11";
+            i_aluControl <= "010";
+
+            assert o_opcode = instr(31 downto 26) report "Opcode mismatch" severity error;
+            assert o_funct = instr(5 downto 0) report "Funct mismatch" severity error;
+            assert o_memAddr = std_logic_vector(unsigned(pc) + 4) report "PC add failed" severity error;
+            
+            wait for c_CLOCK_PERIOD;
+        end procedure states0and1;
+
+    begin   -- stimulus process
+
         -- Signals initialization
         i_irWrite   <= '0';   -- disable write signals
         i_regWrite  <= '0';
@@ -79,28 +116,7 @@ begin
         -- =====================================================================
         -- Instruction lw $s0, 2048($zero)
         
-        -- S0: fetch
-        i_iOrD      <= '0';     -- set memAddr output to PC reg value
-        wait for c_PROP_DELAY;
-        assert o_memAddr = X"00000000" report "PC reset failed" severity error;
-
-        i_irWrite   <= '1';     -- write to instr reg
-        i_aluSrcA   <= '0';     -- PC
-        i_aluSrcB   <= "01";    -- 4
-        i_aluControl <= "010";  -- add
-        i_pcSrc     <= "00";    -- aluOut
-        i_pcWrite   <= '1';     -- set new PC value (PC + 4)
-        i_readData  <= X"8C100800";  -- instruction "read" from mem
-
-        wait for c_CLOCK_PERIOD - c_PROP_DELAY;
-        i_irWrite   <= '0';     -- disable write signals
-        i_pcWrite   <= '0';
-
-        -- S1: decode
-        assert o_opcode = "100011" report "Opcode mismatch" severity error;
-        assert o_memAddr = X"00000004" report "PC add failed" severity error;
-        
-        wait for c_CLOCK_PERIOD;
+        states0and1(X"8C100800", X"00000000");
 
         -- S2: mem addr calc
         i_aluSrcA   <= '1';     -- regA
@@ -112,7 +128,6 @@ begin
         -- S3: mem read
         i_iOrD <= '1';
         wait for c_PROP_DELAY;
-
         -- addr should be 2048 (imm + reg = 2048 + 0 = 2048 = 0x800)
         assert o_memAddr = X"00000800" report "Addr calc failed" severity error;
 
@@ -126,11 +141,31 @@ begin
 
         wait for c_CLOCK_PERIOD;
 
-        -- End of stimulus
 
+        -- =====================================================================
+        -- Instruction  addi $s1, $zero, 1
+
+        states0and1(X"20110001", X"00000004");
+
+        -- S9: addi execute
+        i_aluSrcA   <= '1';     -- regA
+        i_aluSrcB   <= "10";    -- immediate
+        i_aluControl <= "010";  -- add
+
+        wait for c_CLOCK_PERIOD;
+
+        -- S4: addi writeback
+        i_regDst    <= '0';     -- regfile A3 = rt (instruction field, $s0)
+        i_memToReg  <= '0';     -- regfile WD3 = aluOut
+        i_regWrite  <= '1';
+
+        wait for c_CLOCK_PERIOD;
+
+        
+        -- End of stimulus
         r_stop_clock <= true;
         wait;
-    end process;
+    end process stimulus;
     
     clocking: process
     begin
@@ -139,6 +174,6 @@ begin
             wait for c_CLOCK_PERIOD;
         end loop;
         wait;
-    end process;
+    end process clocking;
 
 end architecture bench;
